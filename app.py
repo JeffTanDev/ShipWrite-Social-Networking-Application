3931bbaa9249ed7279c5edeca3170c4da99c6812
Kyle-Hoekstra <kyle.m.s.hoekstra@gmail.com>
2023-10-15 15:22:35 -0700

    Update app.py
    When app is run, redirects automatically to the login page.

--- app.py
+++ app.py
@@ -1,11 +1,39 @@
 from flask import Flask
+from flask import request, url_for, redirect, Blueprint
+from flask import render_template
+
+
+#will eventually help connect the different pages
+bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def start():
+    return redirect(url_for('login'))
+
+@app.route('/login', methods=('GET', 'POST'))
+# Opens the login page 
+def login():
+    if request.method == 'POST':
+        return redirect('CreateUser.html')
+    
+    return render_template('LogIn.html')
+
+
+@app.route('/create_user', methods=('GET', 'POST'))
+# Opens the create user page 
+def open_create_user(name=None):
+    return render_template('CreateUser.html', name=name)
+
+
+@app.route('/home_page', methods=('GET', 'POST'))
+# Opens the home page 
+def open_home_page(name=None):
+    return render_template('HomePage.html', name=name)
+
+
 
 
 if __name__ == '__main__':
