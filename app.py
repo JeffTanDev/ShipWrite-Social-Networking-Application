8af60b289970b46f0c04d48ab6f274206f874ba4
Kyle-Hoekstra <kyle.m.s.hoekstra@gmail.com>
2023-10-18 16:08:20 -0700

    Update app.py
    Basic routing for login and create user pages. No connection to database. Not sure if templates have any code for POST to test routing.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import request, url_for, redirect, Blueprint
+from flask import request, url_for, redirect, Blueprint, flash
 from flask import render_template
 
 
@@ -11,26 +11,76 @@ app = Flask(__name__)
 
 @app.route('/')
 def start():
+    #automatically redirects to login page upon app start up
     return redirect(url_for('login'))
 
+
+
 @app.route('/login', methods=('GET', 'POST'))
-# Opens the login page 
+# Logic for login page
 def login():
+    # open login page
     if request.method == 'POST':
-        return redirect('CreateUser.html')
-    
+        #If receiving a post from the frontend, store username and password data
+        username = request.form['username']
+        password = request.form['password']
+        #database not connected
+        #db = get_db()
+        error = None
+
+        #If username or password field is not filled, return an error
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        
+        if error is None:
+            #Here is where we send the info to the data base to see if the user exists
+            #If user is verified redirect to home page
+            return redirect(url_for(home_page))
+            #if user doesn't exist redirect to register/create user page
+            #If credentials are just wrong, raise error and reprompt login
+            
+
+        flash(error)
+
     return render_template('LogIn.html')
 
 
 @app.route('/create_user', methods=('GET', 'POST'))
 # Opens the create user page 
-def open_create_user(name=None):
+def create_user(name=None):
+    #Works functionally the same to login but will perform different 
+    #checks on the given username and password
+    if request.method == 'POST':
+        #If receiving a post from the frontend, store username and password data
+        username = request.form['username']
+        password = request.form['password']
+        #database not connected
+        #db = get_db()
+        error = None
+
+        #If username or password field is not filled, return an error
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        
+        if error is None:
+            #Here is where we send the info to the data base to see if the user exists
+            #If username is not taken, store data in database and redirect to login
+            #if username is taken, raise error and reprompt
+            return redirect(url_for(login))
+
+        flash(error)
+
     return render_template('CreateUser.html', name=name)
 
 
 @app.route('/home_page', methods=('GET', 'POST'))
 # Opens the home page 
-def open_home_page(name=None):
+def home_page(name=None):
+    #This logic will be more complicated 
     return render_template('HomePage.html', name=name)
 
 
